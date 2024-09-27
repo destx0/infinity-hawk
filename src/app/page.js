121826146b5892b64a130e2d55958a3956b31cdd
destx0 <destisdbest@gmail.com>
2024-09-27 10:57:13 +0530

    added phone

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import HeroSection from '@/components/HeroSection';
 import FeaturesSection from '@/components/FeaturesSection';
+import AppComingSoonSection from '@/components/AppComingSoonSection';
 
 export default function Home() {
 	return (
 		<main>
 			<HeroSection />
 			<FeaturesSection />
+			<AppComingSoonSection />
 			{/* Add other sections or components here */}
 		</main>
 	);
